090d4f6129d1d4848152fc0530c2d27d98501a38
Antonio Carlos Lima <antonioc57@gmail.com>
2024-04-14 15:10:47 -0300

    chore: conventional commits setup

---
+++ .eslintrc.js
@@ -0,0 +1,16 @@
+module.exports = {
+  root: true,
+  extends: ['universe/native', 'universe/shared/typescript-analysis'],
+  overrides: [
+    {
+      files: ['*.ts', '*.tsx', '*.d.ts'],
+      parserOptions: {
+        project: './tsconfig.json',
+      },
+    },
+  ],
+  rules: {
+    // Ensures props and state inside functions are always up-to-date
+    'react-hooks/exhaustive-deps': 'warn',
+  },
+};
--- __tests__/App.test.tsx
+++ __tests__/App.test.tsx
@@ -1,11 +1,11 @@
-import React from 'react';
 import { render } from '@testing-library/react-native';
+import React from 'react';
 
-import App from './App';
+import App from '../App';
 
 describe('<App />', () => {
   it('should show a text Hello Word', () => {
     const { getAllByText } = render(<App />);
-    expect(getAllByText("Hello World")).toBeTruthy();
+    expect(getAllByText('Hello World')).toBeTruthy();
   });
 });
